9c92ebabc651ac267b17d6ef53a3300bc686eba5
K.rahimi <kimiarhimi79@gmail.com>
2024-08-29 23:55:10 +0330

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,28 +1,32 @@
 function FizzBuzz(){
 
-    let input1 =(document.querySelector("#parameter1").value);
+    let input1 = Number((document.querySelector("#parameter1").value));
 
 
-    if( input1 % 3 == 0  &&  input1 % 5 == 0 ){
-
-        console.log("FizzBuzz");
-    }
-
-    else if (input1 % 3 == 0 ){
-        console.log("Fizz");
-    }
-
-    else if(input1 % 5 == 0){
-        console.log("Buzz");
-    }
-    
-    else if (input1 % 3 != 0 && input1 % 5 != 0) {
-
-        console.log("given input Number/Value");
-    }
-
-    else if( typeof(input1) !== Number ) {
-
+    if (isNaN(input1)){
         console.log("NaN");
     }
+    else{
+        if( input1 % 3 == 0  &&  input1 % 5 == 0 ){
+
+            console.log("FizzBuzz");
+        }
+    
+        else if (input1 % 3 == 0 &&  input1 % 5 != 0 ){
+            console.log("Fizz");
+        }
+    
+        else if(input1 % 5 == 0 &&  input1 % 3 != 0 ){
+            console.log("Buzz");
+        }
+        
+        else if (input1 % 3 != 0 && input1 % 5 != 0) {
+    
+            console.log("given input Number/Value");
+        }
+    }
+
 }
+
+// let result1 = FizzBuzz();
+// document.getElementById("result").innerHtml = result1;
